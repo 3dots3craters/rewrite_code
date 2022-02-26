6d3b3be95ac883c87778778f00cfa74d1b1d6c69
3dots3craters <3dots3craters@gmail.com>
2022-02-26 15:40:16 +0900

    Fix: change print message content

--- main.c
+++ main.c
@@ -3,52 +3,53 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hayelee <hayelee@student.42.fr>            +#+  +:+       +#+        */
+/*   By: hayelee <hayelee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/03 16:53:44 by hayelee           #+#    #+#             */
-/*   Updated: 2021/07/05 09:07:51 by hayelee          ###   ########.fr       */
+/*   Updated: 2022/02/26 15:35:34 by hayelee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rewrite_code.h"
 
-int	main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	t_info info;
-	
-	int	i;
+
+	int i;
 
 	i = 1;
 	check_argv(argc, argv);
 	rewrite_file(i, argc, argv, &info);
+	printf("\nRewrite files success!!\n\n");
 	return (0);
 }
 
 void check_argv(int argc, char *argv[])
 {
-    int i;
-    int len;
+	int i;
+	int len;
 
-    i = 0;
-    len = 0;
-    while (++i < argc)
-    {
-        len = ft_strlen(argv[i]);
-        if (argv[i][len - 1] != 'c' && argv[i][len - 2] != '.')
-        {
-            print_error("Error: WRONG FILE\t\t: Check only c files");
-        }
-    }
+	i = 0;
+	len = 0;
+	while (++i < argc)
+	{
+		len = ft_strlen(argv[i]);
+		if (argv[i][len - 1] != 'c' && argv[i][len - 2] != '.')
+		{
+			print_error("Error: CAN'T READ WRONG FILE");
+		}
+	}
 }
 
 void init_info(t_info *info)
 {
 	info->func_tab_start_idx = 0;
-    info->func_tab_end_idx = 0;
-    info->var_tab_idx = 0;
+	info->func_tab_end_idx = 0;
+	info->var_tab_idx = 0;
 }
 
-void	print_error(char *error)
+void print_error(char *error)
 {
 	printf("%s\n", error);
 	exit(1);
--- open_file.c
+++ open_file.c
@@ -3,31 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   open_file.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hayelee <hayelee@student.42.fr>            +#+  +:+       +#+        */
+/*   By: hayelee <hayelee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/05 09:06:12 by hayelee           #+#    #+#             */
-/*   Updated: 2021/07/05 09:06:32 by hayelee          ###   ########.fr       */
+/*   Updated: 2022/02/26 15:35:22 by hayelee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rewrite_code.h"
 
-int	open_origin_file(char *filename)
+int open_origin_file(char *filename)
 {
-	int	fd;
+	int fd;
 
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
-		print_error("Error: OPEN ORIGINAL FILE ERROR");
+		print_error("Error: CAN'T OPEN ORIGINAL FILE");
 	return (fd);
 }
 
-int	open_new_file(char *filename)
+int open_new_file(char *filename)
 {
-	int	fd;
+	int fd;
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0644);
 	if (fd < 0)
-		print_error("Error: OPEN NEW FILE ERROR");
+		print_error("Error: CAN'T OPEN NEW FILE");
 	return (fd);
 }
